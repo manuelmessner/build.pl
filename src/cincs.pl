cb538ec5ec763a16492d0b3385fb05c32be19833
Manuel Johannes Messner <manuel.johannes.messner@hs-furtwangen.de>
2016-04-20 15:37:40 +0200

    Add cincs.pl program

---
+++ src/cincs.pl
@@ -0,0 +1,95 @@
+#! /usr/bin/perl
+
+
+# This file is part of build.pl.
+#
+# build.pl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# build.pl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with build.pl.  If not, see <http://www.gnu.org/licenses/>.
+
+
+use strict;
+use utf8;
+use warnings;
+use feature qw(state say);
+
+use Cwd qw(realpath);
+use File::Basename;
+
+my $g_cfg = {
+    c => {
+        mark  => '^\s*#\s*include\s*"(.*?)\s*"$',
+        subst => sub {
+            local $_ = shift // $_;
+            s/\.h$/.c/;
+            return $_;
+        },
+    },
+    cpp => {
+        mark  => '^\s*#\s*include\s*"(.*?)\s*$"',
+        subst => sub {
+            local $_ = shift // $_;
+            s/\.h$/.cpp/;
+            return $_;
+        },
+    },
+    d => {
+        mark  => '^\s*import\s+(\S+).*;.*$',
+        subst => sub {
+            local $_ = shift // $_;
+            return undef if /^std\./;
+            s/\./\//g;
+            $_ .= '.d';
+            return $_;
+        },
+    },
+    java => {
+        mark => '^\s*import\s+(.*)\s*;\s*$',
+        subst => [''],
+    },
+};
+
+my ($g_ft, $g_fpath) = @ARGV;
+die "usage: " . basename($0) . "<filetype> <file>\n" if not ($g_fpath && $g_ft);
+exit 0 unless exists $g_cfg->{$g_ft};
+
+sub uniq(@) {
+    local %_;
+    return grep { not $_{$_}++ } @_;
+}
+
+sub parse_inc($) {
+    return shift unless $g_cfg->{$g_ft}->{subst};
+    local $_ = $g_cfg->{$g_ft}->{subst}->(shift);
+    return defined $_ && -e $_ ? $_ : undef;
+}
+
+sub get_includes {
+    my $fpath = shift // $_;
+    state %done;
+    return () if exists $done{$fpath};
+    $done{$fpath} = 1;
+
+    open my $f, $fpath or die "Could not open file: $fpath: $!\n";
+    my @incs = map {
+        my $tmp = /$g_cfg->{$g_ft}->{mark}/ ? parse_inc $1 : undef;
+        $tmp ? realpath $tmp : ()
+    } <$f>;
+    close $f;
+
+    push @incs, get_includes($_) for @incs;
+    return uniq @incs;
+}
+
+
+chdir dirname $g_fpath;
+say for get_includes $g_fpath;
